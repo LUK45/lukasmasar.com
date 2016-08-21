82b8bc68da1849ea3a2aa4d6d4a5a9efefb9da2d
LUK45 <lukas.masar@gmail.com>
2016-08-21 15:47:30 +0200

    init

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+jQuery('document').ready(function($) {	
+	/*=========================================*/
+	/*         preloder                       */
+	/*=========================================*/	
+	
+	//preloder ready function code		
+		setTimeout(function(){
+			$('body').addClass('loaded');
+			$('h1').css('color','#222222');
+		}, 500);
+	// end preloder ready function code
+
+		
+	
+
+});
